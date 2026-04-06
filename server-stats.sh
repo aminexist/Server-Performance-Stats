3fdb252dbc0e6b1ef7722cedc7387b0ea7acdc64
Amine ELFALKI <amine.elfalki@gmail.com>
2026-04-06 22:03:44 +0100

    Add memory usage calculation to server-stats.sh

--- server-stats.sh
+++ server-stats.sh
@@ -2,7 +2,7 @@
 cpu_idle=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}' | sed 's/id,//')
 cpu_usage=$(echo "100 - $cpu_idle" | bc)
 echo "Total CPU Usage: $cpu_usage%"
-
+ 
 mem_total=$(free -m | awk '/Mem:/ {print $2}')
 mem_used=$(free -m | awk '/Mem:/ {print $3}')
 mem_percent=$(echo "scale=2; $mem_used/$mem_total*100" | bc)
